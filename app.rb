362e8fab8b2aa1ac1336a0ba9adcfd75e8a0fa31
James Hylins <james.hylins@johnlewis.co.uk>
2019-06-18 16:13:27 +0100

    refactor the code to display the bookmark urls

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+require 'bookmark'
 
 
 class BookmarkManager < Sinatra::Base
   get '/bookmarks' do
+    @array_of_bookmarks = Bookmark.all
     erb :view_bookmarks
   end
   run! if app_file == $0
