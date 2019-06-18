17a55d315bc7807b4ecd01a85aa327ce38f1dab2
James Hylins <james.hylins@johnlewis.co.uk>
2019-06-18 15:53:50 +0100

    display bookmark urls to the user

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,7 +4,13 @@
 
 feature 'Viewing bookmarks' do
   scenario 'user can view a basic welcome page' do
-  visit '/bookmarks'
-  expect(page).to have_content "Bookmark Manager"
- end
+    visit '/bookmarks'
+    expect(page).to have_content "Bookmark Manager"
+   end
+
+   scenario 'user can view a list of bookmarks' do
+     visit '/bookmarks'
+     expect(page).to have_content "www.google.com"
+     expect(page).to have_content "www.bbc.com"
+   end
 end
